f606010b26a28103aaf8c3cc9368543b3bb21457
noanlearn <noviadryn@gmail.com>
2022-08-03 21:58:42 +0700

    until row and Column

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const FirstScreen()
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const FirstScreen());
   }
 }
 
@@ -29,28 +28,69 @@ class FirstScreen extends StatelessWidget {
         title: const Text('FirstScreen'),
         actions: [
           IconButton(
-            onPressed: () {}, 
-            icon: Icon(
-              Icons.search,
-              color: Colors.white
-            ),
+            onPressed: () {},
+            icon: const Icon(Icons.search, color: Colors.white),
           )
         ],
         leading: IconButton(
-          onPressed: () {}, 
-          icon: Icon(
-            Icons.menu,
-            color: Colors.white,
-          )
+            onPressed: () {},
+            icon: const Icon(
+              Icons.menu,
+              color: Colors.white,
+            )),
+      ),
+      /* body: Container(
+        decoration: BoxDecoration(
+          color: Colors.redAccent,
+          // shape: BoxShape.circle   // Shape
+          // boxShadow: [      
+          //   BoxShadow(
+          //     color: Colors.black,
+          //     offset: Offset(3, 6),
+          //     blurRadius: 10
+          //   )
+          // ] // BoxShadow
+          border: Border.all(color: Colors.green, width: 3),
+          borderRadius: BorderRadius.circular(10)
         ),
+        // color: Colors.blue,
+        // width: 200,
+        // height: 100,
+        // padding: EdgeInsets.all(10),
+        // margin: EdgeInsets.all(10),
+        child: const Padding(
+          padding: EdgeInsets.all(30),
+          child: Text('Ini Padding'),
+        )
+        // child: const Text(
+        //   'Hi',
+        //   style: TextStyle(fontSize: 40),
+        //   ),
+      ),*/
+
+      // // Body for Row and Column Example
+      // body: Row(
+      //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //   children: [
+      //     IconButton(icon: Icon(Icons.share), onPressed: () {},),
+      //     IconButton(icon: Icon(Icons.thumb_up), onPressed: () {},),
+      //     IconButton(icon: Icon(Icons.thumb_down), onPressed: () {},),
+
+      //   ],
+      // ),
+
+      body: Column(
+        children: const <Widget>[
+          Text(
+            'Sebuah Judul',
+            style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+          ),
+          Text('Lorem ipsum dolor sit amet'),
+        ],
       ),
-      body: const Center(
-        child: Text('Hello World!'),
-      ),
+
       floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.favorite),
-        onPressed: () {}
-        ),
+          child: const Icon(Icons.favorite), onPressed: () {}),
     );
   }
 }
